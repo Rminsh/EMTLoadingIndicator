f321de14192e9d4b3395637af2178f418d46f5ec
armin <armin536@gmail.com>
2020-05-09 14:04:35 +0430

    Added Swift package manager

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "EMTLoadingIndicator",
+    platforms: [.watchOS(.v4)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "EMTLoadingIndicator",
+            targets: ["EMTLoadingIndicator"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "EMTLoadingIndicator",
+            path: "EMTLoadingIndicator"),
+    ]
+)
--- README.md
+++ README.md
@@ -26,6 +26,18 @@ end
 carthage update --platform watchOS
 ```
 
+### Swift Package Manager
+
+The [Swift Package Manager](https://swift.org/package-manager/) is a tool for automating the distribution of Swift code and is integrated into the `swift` compiler. It is in early development, but Alamofire does support its use on supported platforms.
+
+Once you have your Swift package set up, adding Alamofire as a dependency is as easy as adding it to the `dependencies` value of your `Package.swift`.
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/Rminsh/EMTLoadingIndicator.git", .upToNextMajor(from: "4.0.2"))
+]
+```
+
 ## Usage
 
 ### Initialization
